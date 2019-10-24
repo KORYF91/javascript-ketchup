b8e23f0198d814a2c9e0f765b01a334c6ffb63b4
Kfriedman <Koryf91@gmail.com>
2019-10-23 20:07:59 -0700

    adding some js. here is a few tutorial we ran through

---
+++ index.js
@@ -0,0 +1,72 @@
+// console.log("it would");
+
+//declared varaiables
+var firstName = "kory";
+var age = 28;
+var likesSoccer = true;
+
+var friends = ["Angela", "Jack", "lance"];
+console.log(friends[2]); // Lance
+console.log(friends.length); // 3
+
+console.log(friends[friends.length - 1]); // how to grab lasr object in array when unknown amount of objects.
+console.log(friends.pop()); //pop-method to grab last of the objects in array.
+
+var angela = {
+    name: "Angela",
+    age: 32,
+    profession: "Artist"
+};
+
+console.log(angela.age);
+// console.log(angela[property]);
+
+var fruits = ["banana", "pear", "apple", "cherry", "peach"]
+// console.log(fruits[2]);
+
+
+// var favFoods = { vegetable: "brussel sprouts", meat: "tofu", fruit: "peach", legume: "peanut", grain: "millet" } 
+
+// console.log(favFoods.legume);
+
+// var groceryList = { produce: { fruits: ["bananas", "cherries", "blueberries"], vegetables: ["turnip", "carrot", "spinach"] }, desserts: ["ice cream", "brownie"] }
+
+// console.log(groceryList.desserts[1]);
+// console.log(groceryList.produce.fruits[1]);
+// console.log(groceryList.produce.vegetables.length);
+
+// for loop
+
+// for (var i = 0; i < fruits.length; i++) {
+//    console.log(fruits[i]);
+//     }
+
+// var metals = ["copper", "silver", "tin", "gold", "steel", "aluminum", "iron"]
+
+// for (var i = 0; i < metals.length; i = i + 2) {
+//         // console.log(metals[i]);
+// }
+// var nums = [2, 4, 6, 8, 10];
+
+// for (var i = 0; i < nums.length; i++) {
+//    nums[i] = nums[1] + 1; 
+//    nums[1]
+// }
+// for (var i = 0; i < nums.length; i++) {
+//    nums[i] = nums[1] + 1; 
+// }
+// for (var i = 0; i < nums.length; i = i + 1) {
+//     console.log(nums[i] + 1);
+// }
+
+var num = 4;
+
+// if (num >3) {
+//     console.log("the number is greater than 3");
+// }
+
+// if (num >5) {
+//     console.log("the number is greater than 5");
+// }
+
+var num = 9;
